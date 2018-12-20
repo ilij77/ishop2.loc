a403e37d4add99acd9e0e7ba30cb844794abcd39
Iliya <ilij@ya.ru>
2018-12-21 02:37:20 +0300

    Урок 4

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -22,9 +22,14 @@ class MainController extends AppController
 
     public function indexAction()
     {
+        $brands=\RedBeanPHP\R::find('brand','LIMIT 3');
+        //debug($brands);
+        $hits=\RedBeanPHP\R::find('product',"hit='1' AND status='1' LIMIT 8");
+
 
 
         $this->setMeta('Главная страница', 'Описание', 'Ключевики');
+        $this->set(compact('brands','hits'));
 
     }
 
