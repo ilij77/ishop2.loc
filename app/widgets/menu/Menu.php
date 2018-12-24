2ab137a4ac848f50ea66afc1ddfdd20ce38d5986
Iliya <ilij@ya.ru>
2018-12-24 03:30:00 +0300

    Урок 9

--- app/widgets/menu/Menu.php
+++ app/widgets/menu/Menu.php
@@ -19,6 +19,7 @@ class Menu
     protected $menuHtml;
     protected $tpl;
     protected $container = 'ul';
+    protected $class = 'menu';
     protected $table = 'category';
     protected $cache = 3600;
     protected $cacheKey = 'ishop_menu';
@@ -27,7 +28,7 @@ class Menu
 
     public function __construct($options=[])
     {
-        $this->tpl=__DIR__.'/menu_tpl/menu.php';
+        $this->tpl=__DIR__.'/menu/menu.php';
         $this->getOptions($options);
 
         $this->run();
@@ -55,26 +56,62 @@ class Menu
                 $this->data=$cats=\RedBeanPHP\R::getAssoc("SELECT*FROM {$this->table}");
             }
 
+            $this->tree=$this->getTree();
+            //debug($this->tree);
+            $this->menuHtml=$this->getMenuHtml($this->tree);
+            if (!$this->cache){
+                $cache->set($this->cacheKey,$this->menuHtml,$this->cache);
+            }
+
         }$this->output();
 
     }
 
     protected function output(){
+        $attrs='';
+        if (!empty($attrs)){
+            foreach ($this->attrs as $k=>$v){
+                $attrs.="$k='$v'";
+            }
+        }
+
+        echo "<{$this->container} class='{$this->class}'$attrs>";
+        echo $this->prepend;
         echo $this->menuHtml;
+        echo "</{$this->container}>";
     }
 
 
     protected function  getTree()
     {
+        $tree=[];
+        $data=$this->data;
+        foreach ($data as $id=>&$node){
+            if (!$node['parent_id']){
+                $tree[$id]=&$node;
+            }else{
+                $data[$node['parent_id']]['childs'][$id]=&$node;
+            }
+
+        }
+        return $tree;
 
     }
     protected function getMenuHtml($tree,$tab='')
     {
+        $str='';
+        foreach ($tree as $id=>$category){
+            $str .=$this->catToTemplate($category,$tab,$id);
+        }
+        return $str;
 
     }
 
     protected function catToTemplate($category,$tab,$id)
     {
+        ob_start();
+        require $this->tpl;
+        return ob_get_clean();
 
     }
 
