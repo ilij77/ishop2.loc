8e8857cd85c4b299b0d028fad2e2cef893e763f7
Ilij Work <ilij@ya.ru>
2018-12-28 16:33:06 +0300

    Урок 26++

--- app/models/User.php
+++ app/models/User.php
@@ -11,6 +11,13 @@ namespace app\models;
 
 class User extends AppModel
 {
+    public $attributes=[
+        'login'=>'',
+        'password'=>'',
+        'name'=>'',
+        'email'=>'',
+        'address'=>'',
+            ];
 
 
 }
