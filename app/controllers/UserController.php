3ad65f06e60b800d239fb0c7f3f8a98d566a0e4e
Ilij Work <ilij@ya.ru>
2018-12-28 16:03:03 +0300

    Урок 26+

---
+++ app/controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Илья
+ * Date: 28.12.2018
+ * Time: 15:52
+ */
+
+namespace app\controllers;
+
+
+class UserController extends AppController
+{
+    public function signupAction(){
+
+    }
+    public function loginAction(){
+
+    }
+    public function logoutAction(){
+
+    }
+}
---
+++ app/models/User.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Илья
+ * Date: 28.12.2018
+ * Time: 15:54
+ */
+
+namespace app\models;
+
+
+class User extends AppModel
+{
+
+
+}
