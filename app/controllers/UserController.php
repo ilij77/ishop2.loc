3ef1a26febaec6b0fef22ee386ff7dcab8f29f4f
Iliya <ilij@ya.ru>
2018-12-29 03:58:33 +0300

    Урок 31

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -49,8 +49,25 @@ class UserController extends AppController
 
     public function loginAction(){
 
+        if (!empty($_POST)){
+            $user=new User();
+            if ($user->login()){
+                $_SESSION['success']='Вы успешно авторизованы';
+            }else{
+                $_SESSION['error']='Логин или пароль введены неверно';
+            }
+            redirect();
+
+        }
+
+
+        $this->setMeta('Вход');
     }
     public function logoutAction(){
+
+        if ($_SESSION['user']) unset($_SESSION['user']);
+        $_SESSION['success']='Вы успешно вышли из учетной записи';
+        redirect();
 
     }
 }
