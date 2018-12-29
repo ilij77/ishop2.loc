256629f3fd82964288cedf076816c54dd86052f6
Iliya <ilij@ya.ru>
2018-12-29 17:44:53 +0300

    Урок 35

--- app/models/Order.php
+++ app/models/Order.php
@@ -9,6 +9,11 @@
 namespace app\models;
 
 
+use ishop\App;
+use Swift_Mailer;
+use Swift_Message;
+use Swift_SmtpTransport;
+
 class Order extends AppModel
 {
     public static function saveOrder($data)
@@ -35,7 +40,41 @@ class Order extends AppModel
 
     }
 
-    public static function mailOrder($orler_id,$user_email){
+    public static function mailOrder($order_id,$user_email){
+
+        // Create the Transport
+        $transport = (new Swift_SmtpTransport(App::$app->getProperty('smtp_host'), App::$app->getProperty('smtp_port'), App::$app->getProperty('smtp_protocol')))
+            ->setUsername(App::$app->getProperty('smtp_login'))
+            ->setPassword(App::$app->getProperty('smtp_password'));
+
+
+
+        // Create the Mailer using your created Transport
+        $mailer = new Swift_Mailer($transport);
+
+        //Create a message
+        ob_start();
+        require APP.'/views/mail/mail_order.php';
+        $body=ob_get_clean();
+
+        $message_client = (new Swift_Message("Вы совершили заказ №{$order_id} на сайте " . App::$app->getProperty('shop_name')))
+            ->setFrom([App::$app->getProperty('smtp_login') => App::$app->getProperty('shop_name')])
+            ->setTo($user_email)
+            ->setBody($body, 'text/html');
+
+
+        $message_admin = (new Swift_Message("Сделан заказ №{$order_id}"))
+            ->setFrom([App::$app->getProperty('smtp_login') => App::$app->getProperty('shop_name')])
+            ->setTo(App::$app->getProperty('admin_email'))
+            ->setBody($body, 'text/html');
+
+        $result = $mailer->send($message_client);
+        $result = $mailer->send($message_admin);
+        unset($_SESSION['cart']);
+        unset($_SESSION['cart.qty']);
+        unset($_SESSION['cart.sum']);
+        unset($_SESSION['cart.currency']);
+        $_SESSION['success'] = 'Спасибо за Ваш заказ. В ближайшее время с Вами свяжется менеджер для согласования заказа';
 
     }
 
--- config/params.php
+++ config/params.php
@@ -1,9 +1,12 @@
 <?php
 
 return[
-  'admin_email'=>'admin@mail.com',
+  'admin_email'=>'ilij@yandex.ru',
   'shop_name'=>'Магазин ishop2,loc',
   'pagination'=>3,
-    'smtp_login'=>'',
-    'smtp_password'=>'',
+    'smtp_login'=>'iliya0108@gmail.com',
+    'smtp_password'=>'9947280591',
+    'smtp_host'=>'smtp.gmail.com',
+    'smtp_port'=>'465',
+    'smtp_protocol'=>'ssl',
 ];
