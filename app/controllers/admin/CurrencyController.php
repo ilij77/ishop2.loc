0ca029ba697532e1ef92054a3daa2914825ea80a
Iliya <ilij@ya.ru>
2019-01-14 01:46:04 +0300

    Урок 32

--- app/controllers/admin/CurrencyController.php
+++ app/controllers/admin/CurrencyController.php
@@ -39,5 +39,34 @@ class CurrencyController extends AppController
         $this->setMeta('Новая валюта');
 
     }
+    public function deleteAction(){
+        $id = $this->getRequestID();
+        $currency = \RedBeanPHP\R::load('currency', $id);
+        \RedBeanPHP\R::trash($currency);
+        $_SESSION['success'] = "Валюта удалена";
+        redirect();
+    }
 
+    public function editAction(){
+        if(!empty($_POST)){
+            $id = $this->getRequestID(false);
+            $currency = new Currency();
+            $data = $_POST;
+            $currency->load($data);
+            $currency->attributes['base'] = $currency->attributes['base'] ? '1' : '0';
+            if(!$currency->validate($data)){
+                $currency->getErrors();
+                redirect();
+            }
+            if($currency->update('currency', $id)){
+                $_SESSION['success'] = "Изменения сохранены";
+                redirect();
+            }
+        }
+
+        $id = $this->getRequestID();
+        $currency = \RedBeanPHP\R::load('currency', $id);
+        $this->setMeta("Редактирование валюты {$currency->title}");
+        $this->set(compact('currency'));
+    }
 }
